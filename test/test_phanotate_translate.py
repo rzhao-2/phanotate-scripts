f11144e7d1af5838b159df4e21bdea6543d6b005
rzhao-2 <rossen.zhao@gmail.com>
2022-07-11 15:47:46 +1000

    update unittests

--- test/test_phanotate_translate.py
+++ test/test_phanotate_translate.py
@@ -8,23 +8,23 @@ from Bio import SeqIO
 import extern
 
 path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
-path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin', 'phanotate_circular')
+path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin', 'phanotate-translate')
 
 class Tests(unittest.TestCase):
     maxDiff = None
 
-    def test_phanotate_circular(self):
-        example = [seq for seq in SeqIO.parse(os.path.join(path_to_data, 'NC_009554.1_transcript.fa'), format='fasta')]
+    def test_translate(self):
+        example = [seq for seq in SeqIO.parse(os.path.join(path_to_data, 'NC_009554.1_protein.fa'), format='fasta')]
         with tempfile.NamedTemporaryFile(mode='w', suffix='.fa') as outfile:
-            infile = os.path.join(path_to_data, 'NC_009554.1.fa')
+            infile = os.path.join(path_to_data, 'NC_009554.1_transcript.fa')
             cmd = '%s -i %s -o %s' % (path_to_script, infile, outfile.name)
             extern.run(cmd)
             records = [seq for seq in SeqIO.parse(outfile.name, format='fasta')]
             i = 0
             self.assertEqual(len(example), len(records))
             while i < len(records):
-                self.assertEqual(example[i].seq, records[i].seq)
-                self.assertEqual(example[i].description, records[i].description)
+                self.assertEquals(example[i].seq, records[i].seq)
+                self.assertEquals(example[i].description, records[i].description)
                 i += 1
 
 if __name__ == "__main__":
